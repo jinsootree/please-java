04ff0a4d1cc1c87adb43239a6e630b11058750d8
jinsootree <yjs104710@gmail.com>
2022-04-25 01:30:56 +0900

    message

---
+++ src/Greetins2.java
@@ -0,0 +1,10 @@
+public class Greetins2 {
+    public static void main(String[] args) {
+        String name;
+        name = "김한빛";
+        System.out.println("안녕하세요~ 반갑습니다!. 제 이름은,");
+        System.out.println(name);
+        System.out.println("입니다. 잘 부탁드립니다~");
+
+    }
+}
---
+++ src/exam.java
@@ -0,0 +1,14 @@
+public class exam {
+    public static void main(String[] args) {
+        int midScore; //중간점수
+        int finalScore; //기말점수
+
+        midScore = 68;
+        finalScore = 88;
+
+System.out.println("학기 전 홍팍이의 중간 점수:");
+System.out.println(midScore);
+System.out.println("학기 전 홍팍이의 기말 점수:");
+System.out.println(finalScore);
+    }
+}
