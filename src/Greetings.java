8b787a007b94735d44db27c32b5a673ea1b6e2b6
jinsootree <yjs104710@gmail.com>
2022-04-25 01:03:19 +0900

    changes

---
+++ src/Greetings.java
@@ -0,0 +1,14 @@
+/*
+*자기소개하기
+* 파일명 : Greetings.java
+ */
+//클래스 작성
+public class Greetings {
+    //2 메인 메소드 추가
+    public static void main(String[] args) {
+        //3 문자열 출력
+        System.out.println("안녕 반가워");
+        System.out.println("나는 냥이라고해");
+        System.out.println("잘 부탁할게!");
+    }
+}
