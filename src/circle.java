bfd4853a0d212bd6d1102566dca5608cfc5e4f5e
jinsootree <yjs104710@gmail.com>
2022-04-27 01:21:13 +0900

    message

--- src/circle.java
+++ src/circle.java
@@ -2,12 +2,11 @@ public class circle {
     public static void main(String[] args) {
         //입력값
     int r = Integer.parseInt("4");
-    
+    // 원의 넓이 = (s = pi * r * r) 계산 
     double s = Math.PI * r * r;
 
 
-
-
+// 결과 출력
         System.out.printf("반지름이 %d인 원의 넓이 = %.3f", r, s);
     }
     
