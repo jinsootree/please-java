c7d29d7cd94e178b5fa784e40f8f3fac67ceb285
jinsootree <yjs104710@gmail.com>
2022-04-29 00:07:57 +0900

    change

---
+++ src/method/randomnumber.java
@@ -0,0 +1,15 @@
+package method;
+
+public class randomnumber {
+    public static void main(String[] args) {
+        int n = rolldie();
+        System.out.printf("주사위의 눈:%d", n);
+    }
+
+    public static int rolldie(){
+        double x = 6 * Math.random();
+        int temp =(int) x;
+        return temp + 1;
+    }
+    
+}
