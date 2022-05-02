d7737a60253d830c0c92cc8860052df804a144b8
jinsootree <yjs104710@gmail.com>
2022-05-02 15:20:24 +0900

    message

---
+++ src/method/Random.java
@@ -0,0 +1,18 @@
+package method;
+
+public class Random {
+    public static void main(String[] args) {
+        int x = rolldie();
+
+        System.out.printf("주사위의 눈: %d", x);
+    }
+
+        public static int rolldie() {
+            double r = 6 * Math.random();
+            int temp = (int) r;
+            int n = temp +1;
+            return n;
+        }
+            
+    
+}
