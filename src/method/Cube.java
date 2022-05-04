c005126bf43a51c3f78533ab56dc9eb39d0ea4d2
jinsootree <yjs104710@gmail.com>
2022-05-04 09:36:41 +0900

    m

--- src/method/Cube.java
+++ src/method/Cube.java
@@ -2,7 +2,14 @@ package method;
 
 public class Cube {
     public static void main(String[] args) {
-        System.out.printf("한변의 길이가 3인 정육면체의 부피 : 27");
-    }
+        
     
+    int n = 3;
+    int v = volume(n);
+    
+        System.out.printf("한변의 길이가 %d인 정육면체의 부피 : %d", n, v);
+    }
+    public static int volume(int length){
+        return length * length * length;
+    }    
 }
