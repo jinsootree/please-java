430e764f77ab7e50dad01c24412f3574f81ad667
jinsootree <yjs104710@gmail.com>
2022-05-05 05:08:18 +0900

    m

--- src/method/Pork.java
+++ src/method/Pork.java
@@ -1,5 +1,20 @@
 package method;
 
 public class Pork {
+    public static void main(String[] args) {
+        
+    
+    int n = 3;
+    double x = calculate(n);
+
+    System.out.printf("삼겹살 %d인분의 칼로리 : %.2f", n, x);
+    }
+
+    public static double calculate(int amount){
+        int totalGram = amount * 180;
+        double totalKcal = totalGram * 5.179;
+        return totalKcal;
+    }
+    
     
 }
