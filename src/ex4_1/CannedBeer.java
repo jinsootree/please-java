236c63ad7a1fe2e2341f21b3be7ba64c4c020b41
jinsootree <yjs104710@gmail.com>
2022-05-08 01:18:03 +0900

    m

---
+++ src/ex4_1/CannedBeer.java
@@ -0,0 +1,18 @@
+package ex4_1;
+
+public class CannedBeer {
+    public static void main(String[] args) {
+        Buy(20);
+        Buy(18);
+    }
+
+    //입력 나이에 따라 캔맥주 구매 여부를 결정
+    public static void Buy(int age){
+        if(age >= 19){
+            System.out.printf("%d세, 캔맥주 구매 가능\n" , age);
+        }
+        else {
+            System.out.printf("%d세, 캔맥주 구매 불가\n" , age);
+        }
+    }
+}
