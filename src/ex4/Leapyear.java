0cbafb49b4c35232463cb23f995f36d132899232
jinsootree <yjs104710@gmail.com>
2022-05-11 01:25:39 +0900

    m

---
+++ src/ex4/Leapyear.java
@@ -0,0 +1,9 @@
+package ex4;
+
+public class Leapyear {
+    public static void main(String[] args) {
+        System.out.printf("2020년은 윤년입니까? true\n");
+        System.out.printf("1900년은 윤년입니까? false\n");
+        System.out.printf("2000년은 윤년입니까? true\n");
+    }
+}
