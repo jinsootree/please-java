eadef6816c4f95f98c1bf35d047f48be70c4eb89
jinsootree <yjs104710@gmail.com>
2022-05-12 01:06:43 +0900

    m

--- src/ex4/Leapyear.java
+++ src/ex4/Leapyear.java
@@ -6,18 +6,19 @@ public class Leapyear {
         System.out.printf("1900년은 윤년입니까? %b\n", isLeapyear(1900));
         System.out.printf("2000년은 윤년입니까? %b\n", isLeapyear(2000));
     }
-    //연도를 입력받아, 윤년 여부를 반환하는 메소드
-    public static boolean isLeapyear(int year){
-boolean result = false;
-if ( year % 4 == 0){
-    result =  true;
-if ( year % 100 == 0){
-        result = false;
-if ( year % 1000 == 0){
-    result = true;
-}
-}    
-}   
-return result;
-}
+
+    // 연도를 입력받아, 윤년 여부를 반환하는 메소드
+    public static boolean isLeapyear(int year) {
+        boolean result = false;
+        if (year % 4 == 0) {
+            result = true;
+            if (year % 100 == 0) {
+                result = false;
+                if (year % 1000 == 0) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
 }
