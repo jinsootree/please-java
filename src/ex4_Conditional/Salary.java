40d0067a3663b1f4ad5ca4a23758a6c67780a934
jinsootree <yjs104710@gmail.com>
2022-05-14 00:17:54 +0900

    m

---
+++ src/ex4_Conditional/Salary.java
@@ -0,0 +1,15 @@
+import javax.naming.spi.DirStateFactory.Result;
+
+public class Salary {
+    public static void main(String[] args) {
+        printSalary(10000, 160);
+        printSalary(15000, 175);
+        printSalary(9000, 180);
+        printSalary(13000, 190);
+
+    }
+public static void printSalary(int wage, int bounus){
+
+    System.out.printf("");
+}    
+}
