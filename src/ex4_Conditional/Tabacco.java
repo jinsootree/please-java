6bc17db3cb4f57a649b3a64060b2e70dfe38df45
jinsootree <yjs104710@gmail.com>
2022-05-15 16:46:52 +0900

    m

---
+++ src/ex4_Conditional/Tabacco.java
@@ -0,0 +1,20 @@
+package ex4_Conditional;
+
+public class Tabacco {
+    public static void main(String[] args) {
+        buy(15);
+        buy(19);
+        buy(22);
+
+    }
+    public static void buy(int age){
+        String result = "";
+
+        if(age >= 19 ) {
+            result = "0"; // 19세 이상은 판매 허용
+        } else {
+            result = "X"; // 19세 미만은 판매 금지!
+        }
+        System.out.printf("%d세 => 담배판매(%s)\n", age, result);
+    }
+}
