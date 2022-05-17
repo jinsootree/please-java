2626691a11d37005761b2fc74bb67e6003cead03
jinsootree <yjs104710@gmail.com>
2022-05-17 15:49:09 +0900

    m

--- src/ex4_Conditional/Scholarship.java
+++ src/ex4_Conditional/Scholarship.java
@@ -10,16 +10,18 @@ public class Scholarship {
     //이름과 수학 및 영어 점수를 입력받아, 장학금 수령 여부를 출력
     
     public static void printScholarship(String name, int math, int eng){
+        //변수 생성
         String result = "";
 
-    if(math>=90 && eng >=90){
+    //조건에 따른 값 변경
+    if(math>=90 && eng >=90){/* 전액장학금 조건*/
         result ="전액 장학금";
-    } else if (math>=90 || eng>=90) {
+    } else if (math>=90 || eng>=90) {/*반액 장학금 조건*/
         result = "반액 장학금";
     } else {
         result = "다음 기회에";
     }
-
+//결과 출력
 System.out.printf("%s => %s\n", name, result);
     }   
 }
