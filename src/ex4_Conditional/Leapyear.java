945f483da5cb833bed84057f1e4322be2a59cd84
jinsootree <yjs104710@gmail.com>
2022-05-19 23:55:43 +0900

    m

--- src/ex4_Conditional/Leapyear.java
+++ src/ex4_Conditional/Leapyear.java
@@ -4,12 +4,13 @@ public class Leapyear {
     public static void main(String[] args) {
        int input =  Integer.parseInt(args[0]); // 입력값 받기
        Boolean ouput = isLeapyear(input); // 윤년여부 계산
+       System.out.printf("%d년은 윤년입니까? %s", input, ouput);
     }
 
     // 연도를 입력받아, 윤년 여부를 반환하는 메소드
-    public static boolean isLeapyear(int year) {
-        boolean result = false;
-        if (year % 4 == 0) {
+    public static boolean isLeapyear(int year) { //윤년 판별 메소드
+        boolean result = false; // 변수 생성
+        if (year % 4 == 0) { // 조건문 처리 (윤년 여부 판별)
             result = true;
             if (year % 100 == 0) {
                 result = false;
