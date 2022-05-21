72aaabab5ffe4915495d39caf57988258b974712
jinsootree <yjs104710@gmail.com>
2022-05-21 18:28:38 +0900

    m

--- src/반복문/WhileStatement.java
+++ src/반복문/WhileStatement.java
@@ -2,13 +2,15 @@ package 반복문;
 
 public class WhileStatement {
     public static void main(String[] args) {
+        // 입력
         int startNum = Integer.parseInt("5");
+        //카운트다운 출력
         countDown(startNum);
     }
 
     public static void countDown(int num){
         System.out.println("카운트다운을 시작합니다..");
-        while (num > 0 ){
+        while (num >= 0 ){
             System.out.printf("%d..\n",num);
             num --;
         }
