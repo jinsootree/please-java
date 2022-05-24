c98ef6603d74ae2b274e80790b43cf37dd848f14
jinsootree <yjs104710@gmail.com>
2022-05-24 19:03:55 +0900

    m

--- src/반복문/Continue.java
+++ src/반복문/Continue.java
@@ -4,19 +4,23 @@ public class Continue {
     public static void main(String[] args) {
         printSum(1,10);
     }
+    //start부터 end까지의 총합을 계산 및 출력
     public static void printSum(int start, int end) {
+        //변수 생성
         int Sum = 0;
 
         for (int i = start; i <= end; i++){
+            //3의 배수인 경우, 계산 안 함
             if (i % 3 == 0){
-                continue;
+                continue; // 다음 반복으로 이동
             }
             System.out.printf("%d", i);
             Sum += i;
-            if(i == end){
-             break; }
+            if(i == end){//i==end라면
+             break; }//밑의 줄을 실행하지 않고 강제 탈출
             System.out.printf("+");
         }
+        //결과 출력
         System.out.printf("\n => %d", Sum);
     }
     
