27a56be8979a37ce377d2adf19b4d0077a76dec8
jinsootree <yjs104710@gmail.com>
2022-05-25 10:44:05 +0900

    m

---
+++ src/반복문/Matrix.java
@@ -0,0 +1,12 @@
+package 반복문;
+
+public class Matrix {
+    public static void main(String[] args) {
+        for (int row = 0; row < 7; row++){
+        for (int col = 0; col < 4; col++){
+            System.out.printf("* ");
+        }
+        System.out.println( );
+        }
+    }
+}
