a0458391eb80849f33d12c8764ba9841b47b1d02
jinsootree <yjs104710@gmail.com>
2022-05-27 17:50:17 +0900

    m

---
+++ src/배열/grade.java
@@ -0,0 +1,13 @@
+package 배열;
+
+public class grade {
+    public static void main(String[] args) {
+        double grade [] = {3.45, 2.82, 3.85, 3.94};
+
+        System.out.printf("1학년: %.2f\n", grade[0]);
+        System.out.printf("2학년: %.2f\n", grade[1]);
+        System.out.printf("3학년: %.2f\n", grade[2]);
+        System.out.printf("4학년: %.2f\n", grade[3]);
+
+    }
+}
