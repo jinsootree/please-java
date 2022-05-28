47f1d7e2b5bb5eabc29c6d7a703291bbc06021d3
jinsootree <yjs104710@gmail.com>
2022-05-28 18:57:25 +0900

    m

---
+++ src/배열/names.java
@@ -0,0 +1,13 @@
+package 배열;
+
+public class names {
+    public static void main(String[] args) {
+        String [] names =  {"sam","kate","john", "jenny"};
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
+    }
+}
