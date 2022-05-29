21b29d115928642f0e569c7ce3377946dcf24136
jinsootree <yjs104710@gmail.com>
2022-05-29 21:39:36 +0900

    m

---
+++ src/배열/Americano.java
@@ -0,0 +1,17 @@
+package 배열;
+
+public class Americano {
+    public static void main(String[] args) {
+        int sales [] = new int[5];
+        sales[0] = 12;
+        sales[1] = 18;
+        sales[2] = 22;
+        sales[3] = 24;
+        sales[4] = 36;
+
+        int sum = sales[0] + sales[1] + sales[2] + sales[3] + sales[4];
+
+        System.out.printf("총 판매량 : %d잔\n", sum);
+
+    }
+}
