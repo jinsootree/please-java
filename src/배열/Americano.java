226d567cc10ef1597fb9fbc41ffab3c98829295d
jinsootree <yjs104710@gmail.com>
2022-05-29 21:43:12 +0900

    m

--- src/배열/Americano.java
+++ src/배열/Americano.java
@@ -2,12 +2,13 @@ package 배열;
 
 public class Americano {
     public static void main(String[] args) {
-        int sales [] = new int[5];
-        sales[0] = 12;
-        sales[1] = 18;
-        sales[2] = 22;
-        sales[3] = 24;
-        sales[4] = 36;
+        int sales [] = {12, 18, 22, 24, 36};
+        //int sales[] = new int[5];
+        //sales[0] = 12;
+        //sales[1] = 18;
+        //sales[2] = 22;
+        //sales[3] = 24;
+        //sales[4] = 36;
 
         int sum = sales[0] + sales[1] + sales[2] + sales[3] + sales[4];
 
