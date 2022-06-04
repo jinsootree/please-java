50b4e617a16f0404846fe3642fe142bb60ecc99a
jinsootree <yjs104710@gmail.com>
2022-06-04 13:39:46 +0900

    m

--- src/배열/dailyuser.java
+++ src/배열/dailyuser.java
@@ -2,10 +2,11 @@ package 배열;
 
 public class dailyuser {
     public static void main(String[] args) {
+        //1.배열 만들기
         int [] counts = {581,512,527,495,423,141,236};
-
+        //2. 메소드 호출해서 계산
         double result = average(counts);
-
+        //3. result 출력
         System.out.printf("하루 평균 사용자 : %.2f명",result);
     }
 
