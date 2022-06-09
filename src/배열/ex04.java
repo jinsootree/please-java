435151600f225952e8fd3cfb0f93ea96732257af
jinsootree <yjs104710@gmail.com>
2022-06-09 10:36:56 +0900

    m

---
+++ src/배열/ex04.java
@@ -0,0 +1,11 @@
+package 배열;
+
+public class ex04 {
+    public static void main(String[] args) {
+        int [][] arr = {
+            {1,2,3,4,5},
+            {6,7,8,9,0},
+            {10,20,30,40,50}
+        }; System.out.println(arr[0][4] + arr[1][3]+ arr[2][2]);
+    } 
+}
