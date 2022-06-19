62037649fa7dc7a8a247b4377ca383acb5ae4f34
jinsootree <yjs104710@gmail.com>
2022-06-19 18:41:48 +0900

    m

--- src/OOP/GorokeTest.java
+++ src/OOP/GorokeTest.java
@@ -15,13 +15,18 @@ public class GorokeTest {
 
 class Goroke {
 
+//필드
+
     String name;
     int price;
+
+//생성자
 
     Goroke(String n, int p){
        name = n;
        price = p;
     }
+//메소드 - 고로케 객체의 필드 정보를 문자열로 반환
 
     String str(){
         return String.format("고로케 {이름 : %s, 가격 : %d}", name, price);
