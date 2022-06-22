cd1725ff5ab82b56e92a5836c2d50923b090c32b
jinsootree <yjs104710@gmail.com>
2022-06-23 05:05:32 +0900

    m

---
+++ src/OOP/HeroTest1.java
@@ -0,0 +1,38 @@
+package OOP;
+
+public class HeroTest1 {
+    public static void main(String[] args) {
+
+        Hero1 zoro = new Hero1("조로", 150);
+        Hero1 sangdi = new Hero1("상디", 160);
+
+        zoro.punch(sangdi);
+        zoro.punch(sangdi);
+        sangdi.punch(zoro);
+
+        
+    }
+}
+
+class Hero1 {
+
+    String name;
+    int hp;
+
+    Hero1(String s, int i) {
+        name = s;
+        hp = i;
+    }
+
+    void punch(Hero1 enemy) {
+        System.out.printf(" [%s]의 펀치", name);
+
+        System.out.printf("%s의 체력: %d -> ", enemy.name, enemy.hp);
+        enemy.hp -= 10;
+        System.out.printf("%d\n ", enemy.hp);
+
+
+
+    }
+
+}
