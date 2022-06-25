8df843420c92ee05ec7e681dad11b0007c37f862
jinsootree <yjs104710@gmail.com>
2022-06-25 21:34:38 +0900

    m

--- src/OOP/EmployeeTest.java
+++ src/OOP/EmployeeTest.java
@@ -2,15 +2,22 @@ package OOP;
 
 public class EmployeeTest {
     public static void main(String[] args) {
+//배열 생성
+
         int [] hours0 =  {2,4,3,4,5,8,8};
         int [] hours1=  {7,3,4,3,3,4,4};
 
         
+//객체 생성
 
         Employee e0 = new Employee("직원0", hours0);
         Employee e1 = new Employee("직원1", hours1);
 
+//객체 배열 생성
+
         Employee [] emplyoees = {e0, e1};
+
+// 출력
         
         for (int i = 0; i < emplyoees.length; i++) {
             emplyoees[i].printToealhours();
