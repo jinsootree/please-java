6069e04c316d7d90db76d0ac963724a566fdc354
jinsootree <yjs104710@gmail.com>
2022-06-25 21:32:59 +0900

    m

--- src/OOP/EmployeeTest.java
+++ src/OOP/EmployeeTest.java
@@ -3,10 +3,14 @@ package OOP;
 public class EmployeeTest {
     public static void main(String[] args) {
         int [] hours0 =  {2,4,3,4,5,8,8};
+        int [] hours1=  {7,3,4,3,3,4,4};
+
+        
 
         Employee e0 = new Employee("직원0", hours0);
+        Employee e1 = new Employee("직원1", hours1);
 
-        Employee [] emplyoees = {e0};
+        Employee [] emplyoees = {e0, e1};
         
         for (int i = 0; i < emplyoees.length; i++) {
             emplyoees[i].printToealhours();
