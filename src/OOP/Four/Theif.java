143815e513d783edff8c1baac1bda3b5ce863b19
jinsootree <yjs104710@gmail.com>
2022-07-02 00:40:10 +0900

    m

--- src/OOP/Four/Theif.java
+++ src/OOP/Four/Theif.java
@@ -1,30 +1,31 @@
 package OOP.Four;
 
 public class Theif {
-    // 메인 메소드
-    public static void main(String[] args) {
-      // 지갑 객체 생성
-      Wallet wallet = new Wallet(30000);
-      System.out.print(wallet.toString());
-      // 지갑에서 돈을 모두 꺼냄
-      Theif.steal(wallet);
-      // 지갑 확인
-      System.out.println(" -> " + wallet.toString());
-    }
-    // 클래스 메소드
-    public static void steal(Wallet target) {
-      target.money = 0;
-    }
+  // 메인 메소드
+  public static void main(String[] args) {
+    // 지갑 객체 생성
+    Wallet wallet = new Wallet(30000);
+    System.out.print(wallet.toString());
+    // 지갑에서 돈을 모두 꺼냄
+    Theif.steal(wallet);
+    // 지갑 확인
+    System.out.println(" -> " + wallet.toString());
   }
-  class Wallet {
-    // 필드(인스턴스 변수)
-    private int money;
-    // 생성자
-    public Wallet(int i) {
-      money = i;
-    }
-    // 메소드
-    public String toString() {
-      return String.format("Wallet { money: %d }", money);
-    }
+  // 클래스 메소드
+  public static void steal(Wallet target) {
+    target.money = 0;
   }
+}
+class Wallet {
+  // 필드(인스턴스 변수)
+  private int money;
+  // 생성자
+  public Wallet(int i) {
+    money = i;
+  }
+  // 메소드
+  public String toString() {
+    return String.format("Wallet { money: %d }", money);
+  }
+}
+
