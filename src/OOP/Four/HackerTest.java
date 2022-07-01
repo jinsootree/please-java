a896bd2bcf544547846a2b29762fc7efe64261b2
jinsootree <yjs104710@gmail.com>
2022-07-02 00:39:36 +0900

    m

--- src/OOP/Four/HackerTest.java
+++ src/OOP/Four/HackerTest.java
@@ -32,7 +32,7 @@ public class HackerTest {
   class Hacker {
     // 클래스 메소드 - 계좌의 잔액을 0원으로 변경
     public static void malcious(Account account) {
-      /* 1. 파라미터 계좌의 잔액을 0으로 변경하세요. */
+      /* 1. 파라미터 계좌의 잔액을 0으로 변경. */
       account.balance = 0;
     }
   }
